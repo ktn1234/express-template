dd4d780be67ec55aca6aef07fa809f0b3c2450a6
Kevin Nguyen <ktn1234@yahoo.com>
2023-01-14 21:21:33 -0600

    send 200 status

--- src/router/base.ts
+++ src/router/base.ts
@@ -2,6 +2,6 @@ import { Router, Request, Response } from "express";
 
 export default (router: Router): void => {
   router.get("/", (_: Request, res: Response) => {
-    return res.status(200);
+    return res.status(200).send();
   });
 };
